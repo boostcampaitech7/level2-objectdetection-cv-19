0b1a69f96d2c9fabb7b9e63408733272292f5bce
김범조 <8eomio@gimbeomjoui-MacBookPro.local>
2024-10-27 17:18:04 +0900

    refactor: refactoring directory

--- baseline/mmdetection/configs/grounding_dino/grounding_dino_swin-t_finetune_16xb2_1x_coco.py
+++ baseline/mmdetection/configs/grounding_dino/grounding_dino_swin-t_finetune_16xb2_1x_coco.py
@@ -1,9 +1,9 @@
 _base_ = [
     '../_base_/datasets/coco_detection.py',
-    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
+    '../_base_/schedules/schedule_1x.py', 
+    '../_base_/default_runtime.py'
 ]
 load_from = 'https://download.openmmlab.com/mmdetection/v3.0/grounding_dino/groundingdino_swint_ogc_mmdet-822d7e9d.pth'  # noqa
-lang_model_name = 'bert-base-uncased'
 
 model = dict(
     type='GroundingDINO',
@@ -17,14 +17,8 @@ model = dict(
         bgr_to_rgb=True,
         pad_mask=False,
     ),
-    language_model=dict(
-        type='BertModel',
-        name=lang_model_name,
-        pad_to_max=False,
-        use_sub_sentence_represent=True,
-        special_tokens_list=['[CLS]', '[SEP]', '.', '?'],
-        add_pooling_layer=False,
-    ),
+    # language_model 제거
+    language_model=dict(),
     backbone=dict(
         type='SwinTransformer',
         embed_dims=96,
@@ -53,58 +47,39 @@ model = dict(
     encoder=dict(
         num_layers=6,
         num_cp=6,
-        # visual layer config
         layer_cfg=dict(
             self_attn_cfg=dict(embed_dims=256, num_levels=4, dropout=0.0),
-            ffn_cfg=dict(
-                embed_dims=256, feedforward_channels=2048, ffn_drop=0.0)),
-        # text layer config
-        text_layer_cfg=dict(
-            self_attn_cfg=dict(num_heads=4, embed_dims=256, dropout=0.0),
-            ffn_cfg=dict(
-                embed_dims=256, feedforward_channels=1024, ffn_drop=0.0)),
-        # fusion layer config
-        fusion_layer_cfg=dict(
-            v_dim=256,
-            l_dim=256,
-            embed_dim=1024,
-            num_heads=4,
-            init_values=1e-4),
+            ffn_cfg=dict(embed_dims=256, feedforward_channels=2048, ffn_drop=0.0)),
     ),
     decoder=dict(
         num_layers=6,
         return_intermediate=True,
         layer_cfg=dict(
-            # query self attention layer
             self_attn_cfg=dict(embed_dims=256, num_heads=8, dropout=0.0),
-            # cross attention layer query to text
-            cross_attn_text_cfg=dict(embed_dims=256, num_heads=8, dropout=0.0),
-            # cross attention layer query to image
+            # cross_attn_text_cfg 제거, 이미지 관련 cross_attn_cfg만 남김
             cross_attn_cfg=dict(embed_dims=256, num_heads=8, dropout=0.0),
-            ffn_cfg=dict(
-                embed_dims=256, feedforward_channels=2048, ffn_drop=0.0)),
-        post_norm_cfg=None),
+            ffn_cfg=dict(embed_dims=256, feedforward_channels=2048, ffn_drop=0.0)),
+        post_norm_cfg=None
+    ),
     positional_encoding=dict(
         num_feats=128, normalize=True, offset=0.0, temperature=20),
     bbox_head=dict(
         type='GroundingDINOHead',
-        num_classes=80,
+        num_classes=10,  # 10개의 분류 클래스
         sync_cls_avg_factor=True,
-        contrastive_cfg=dict(max_text_len=256, log_scale=0.0, bias=False),
+        contrastive_cfg={},  # contrastive 설정 제거
         loss_cls=dict(
             type='FocalLoss',
             use_sigmoid=True,
             gamma=2.0,
             alpha=0.25,
-            loss_weight=1.0),  # 2.0 in DeformDETR
+            loss_weight=1.0),
         loss_bbox=dict(type='L1Loss', loss_weight=5.0),
         loss_iou=dict(type='GIoULoss', loss_weight=2.0)),
-    dn_cfg=dict(  # TODO: Move to model.train_cfg ?
+    dn_cfg=dict(  
         label_noise_scale=0.5,
-        box_noise_scale=1.0,  # 0.4 for DN-DETR
-        group_cfg=dict(dynamic=True, num_groups=None,
-                       num_dn_queries=100)),  # TODO: half num_dn_queries
-    # training and testing settings
+        box_noise_scale=1.0,  
+        group_cfg=dict(dynamic=True, num_groups=None, num_dn_queries=100)), 
     train_cfg=dict(
         assigner=dict(
             type='HungarianAssigner',
@@ -134,8 +109,6 @@ train_pipeline = [
             [
                 dict(
                     type='RandomChoiceResize',
-                    # The radio of all image in train dataset < 7
-                    # follow the original implement
                     scales=[(400, 4200), (500, 4200), (600, 4200)],
                     keep_ratio=True),
                 dict(
@@ -154,8 +127,7 @@ train_pipeline = [
     dict(
         type='PackDetInputs',
         meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape',
-                   'scale_factor', 'flip', 'flip_direction', 'text',
-                   'custom_entities'))
+                   'scale_factor', 'flip', 'flip_direction'))
 ]
 
 test_pipeline = [
@@ -165,7 +137,7 @@ test_pipeline = [
     dict(
         type='PackDetInputs',
         meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape',
-                   'scale_factor', 'text', 'custom_entities'))
+                   'scale_factor'))
 ]
 
 train_dataloader = dict(
@@ -187,7 +159,7 @@ optim_wrapper = dict(
         'backbone': dict(lr_mult=0.1)
     }))
 # learning policy
-max_epochs = 12
+max_epochs = 100
 param_scheduler = [
     dict(
         type='MultiStepLR',
@@ -198,7 +170,4 @@ param_scheduler = [
         gamma=0.1)
 ]
 
-# NOTE: `auto_scale_lr` is for automatically scaling LR,
-# USER SHOULD NOT CHANGE ITS VALUES.
-# base_batch_size = (16 GPUs) x (2 samples per GPU)
 auto_scale_lr = dict(base_batch_size=32)
